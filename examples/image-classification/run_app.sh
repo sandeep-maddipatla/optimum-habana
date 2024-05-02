16923f1747e7a8fba98aa77dc41043f6b85082ef
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2024-05-02 21:12:22 +0000

    Add and Use local Preprocessor config file
    - Switch cmd line to use local preprocessor config file
    - Works for both Method-A and Method-B

--- examples/image-classification/run_app.sh
+++ examples/image-classification/run_app.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-IMAGE_PROCESSOR_NAME=${1:-https://huggingface.co/timm/resnetv2_50x1_bit.goog_in21k/raw/main/config.json}
 
-# Corresponds to Method-A reported in GS-123
+# Use local preprocessor_config.json by defaul
+IMAGE_PROCESSOR_NAME=${1:-$(pwd)/preprocessor_config.json}
+
+# Switch to below to use the online config file instead
+# IMAGE_PROCESSOR_NAME=${1:-https://huggingface.co/timm/resnetv2_50x1_bit.goog_in21k/raw/main/config.json}
+
+# Works for both Method-A and Method-B reported in GS-123
 CMDLINE="python run_image_classification.py \
      --model_name_or_path  timm/resnetv2_50x1_bit.goog_in21k \
      --dataset_name cifar10 \
