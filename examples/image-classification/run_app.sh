685b1216bcc6c5a71d7f8c5b79c6f6eb3ebbfc6e
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2024-05-08 21:20:56 +0000

    Add configurability to run_app script

--- examples/image-classification/run_app.sh
+++ examples/image-classification/run_app.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# Use local preprocessor_config.json by defaul
-IMAGE_PROCESSOR_NAME=${1:-$(pwd)/preprocessor_config.json}
 
-# Switch to below to use the online config file instead
-# IMAGE_PROCESSOR_NAME=${1:-https://huggingface.co/timm/resnetv2_50x1_bit.goog_in21k/raw/main/config.json}
+# Use the online config file by default
+IMAGE_PROCESSOR_NAME=${1:-https://huggingface.co/timm/resnetv2_50x1_bit.goog_in21k/raw/main/config.json}
+
+# Switch to below to use local preprocessor_config.json by default
+# IMAGE_PROCESSOR_NAME=${1:-$(pwd)/preprocessor_config.json}
+
+MODEL=${MODEL:-timm/resnetv2_50x1_bit.goog_in21k}
+EPOCHS=${EPOCHS:-5}
+OUTDIR=${OUTDIR:-/tmp/outputs}
+
+rm -rf ${OUTDIR}
 
 # Works for both Method-A and Method-B reported in GS-123
 CMDLINE="python run_image_classification.py \
-     --model_name_or_path  timm/resnetv2_50x1_bit.goog_in21k \
+     --model_name_or_path  ${MODEL} \
      --dataset_name cifar10 \
-     --output_dir /tmp/outputs/  \
+     --output_dir ${OUTDIR}  \
      --remove_unused_columns False  --image_column_name img \
-     --do_train --do_eval --learning_rate 2e-4 --num_train_epochs 5 \
+     --do_train --do_eval --learning_rate 2e-4 --num_train_epochs ${EPOCHS} \
      --per_device_train_batch_size 64 --per_device_eval_batch_size 64 \
      --evaluation_strategy epoch  --save_strategy epoch \
      --load_best_model_at_end True \
