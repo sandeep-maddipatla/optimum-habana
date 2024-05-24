25a5d8c3d0a929289b0e9c69859177d2ea6e3e50
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2024-05-24 07:47:49 +0000

    Updates to test script

--- examples/object-detection/test.py
+++ examples/object-detection/test.py
@@ -3,3 +3,13 @@ import sys
 
 dataset = load_dataset(sys.argv[1])
 print(dataset)
+labels = dataset["train"].features[sys.argv[2]].names
+print(labels)
+
+label2id, id2label = {}, {}
+for i, label in enumerate(labels):
+    label2id[label] = str(i)
+    id2label[str(i)] = label
+
+print(label2id)
+print(id2label)
