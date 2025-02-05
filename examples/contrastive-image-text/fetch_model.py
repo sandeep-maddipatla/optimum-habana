26067974471ae5a314bd6d94bf6e98014ea19602
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2025-02-05 07:57:15 +0000

    Add scripts for model fetch and deepspeed config

---
+++ examples/contrastive-image-text/fetch_model.py
@@ -0,0 +1,18 @@
+from transformers import (
+    VisionTextDualEncoderModel,
+    VisionTextDualEncoderProcessor,
+    AutoTokenizer,
+    AutoImageProcessor
+)
+
+model = VisionTextDualEncoderModel.from_vision_text_pretrained(
+    "openai/clip-vit-base-patch32", "roberta-base"
+)
+
+tokenizer = AutoTokenizer.from_pretrained("roberta-base")
+image_processor = AutoImageProcessor.from_pretrained("openai/clip-vit-base-patch32")
+processor = VisionTextDualEncoderProcessor(image_processor, tokenizer)
+
+# save the model and processor
+model.save_pretrained("clip-roberta")
+processor.save_pretrained("clip-roberta")
