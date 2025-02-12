d1491bff3738dddff758bcc54018d333030a7ad7
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2025-02-12 19:12:20 +0000

    Reproducer for Hs-3977

---
+++ hs3977/test.py
@@ -0,0 +1,23 @@
+import torch
+import random
+from habana_frameworks.torch.hpex.normalization import FusedRMSNorm
+
+torch.use_deterministic_algorithms(True)
+torch.manual_seed(0)
+random.seed(0)
+
+HPUFusedRMSNorm = FusedRMSNorm
+
+weight = torch.rand([2,3], dtype=torch.float).to('hpu')
+test_tensor = torch.rand([2,3], dtype=torch.float).to('hpu')
+
+print(f'weight = {weight}')
+print(f'test_tensor = {test_tensor}')
+
+print("FusedRMSNorm fast path")
+result = HPUFusedRMSNorm.apply(test_tensor, weight, 1e-06, True, 0, True)
+print(result)
+
+print("FusedRMSNorm default path")
+result = HPUFusedRMSNorm.apply(test_tensor, weight, 1e-06, True, 0, False)
+print(result)
