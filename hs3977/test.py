c68f378c8bfdf894f2076ec7a5eb4bfe78f44b34
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2025-03-05 05:53:50 +0000

    Simplify test scripts for hs-3977

--- hs3977/test.py
+++ hs3977/test.py
@@ -6,18 +6,12 @@ torch.use_deterministic_algorithms(True)
 torch.manual_seed(0)
 random.seed(0)
 
-HPUFusedRMSNorm = FusedRMSNorm
-
 weight = torch.rand([2,3], dtype=torch.float).to('hpu')
 test_tensor = torch.rand([2,3], dtype=torch.float).to('hpu')
 
 print(f'weight = {weight}')
 print(f'test_tensor = {test_tensor}')
 
-#print("FusedRMSNorm fast path")
-#result = HPUFusedRMSNorm.apply(test_tensor, weight, 1e-06, True, 0, True)
-#print(result)
-
 print("FusedRMSNorm default path")
-result = HPUFusedRMSNorm.apply(test_tensor, weight, 1e-06, True, 0, False)
+result = FusedRMSNorm.apply(test_tensor, weight, 1e-06, True, 0, False)
 print(result)
