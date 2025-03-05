6c8ed76dd427d263724c31ca369b907bfa090967
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2025-03-05 05:53:50 +0000

    Simplify test scripts for hs-3977

--- hs3977/test_compile.py
+++ hs3977/test_compile.py
@@ -2,16 +2,13 @@ import torch
 import random
 from habana_frameworks.torch.hpex.normalization import FusedRMSNorm
 
-HPUFusedRMSNorm = FusedRMSNorm
-
 def RmsNormTCFunction(test_tensor, weight, eps=1e-06):
-    result = HPUFusedRMSNorm.apply(test_tensor, weight, eps, use_stages=True, bwd_mode=0, fast_math=False)
+    result = FusedRMSNorm.apply(test_tensor, weight, eps, use_stages=True, bwd_mode=0, fast_math=False)
     return result
 
 torch.use_deterministic_algorithms(True)
 torch.manual_seed(0)
 random.seed(0)
-
 
 weight = torch.rand([2,3], dtype=torch.float).to('hpu')
 test_tensor = torch.rand([2,3], dtype=torch.float).to('hpu')
