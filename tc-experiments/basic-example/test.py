93431453c0bf3df0a8063e1ad69322ffef9b186d
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2025-04-10 17:16:40 +0000

    Revert "Save updated model with linear"
    This reverts commit 02d57a60b8ae40d3eb8ff1aeda40c9e616faf3c8.

--- tc-experiments/basic-example/test.py
+++ tc-experiments/basic-example/test.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import habana_frameworks.torch.core as htcore
 import habana_frameworks.torch.hpu.random as htrandom
 import argparse
@@ -12,53 +11,26 @@ import numpy as np
 class TanhModel(nn.Module):
     def __init__(self):
         super(TanhModel, self).__init__()
-        self.linear = nn.Linear(4, 1)
         self.activation = nn.Tanh()
 
     def forward(self, x):
-        y = self.linear(x)
-        y = self.activation(y)
-        return y
-
-def is_lazy_mode():
-    return os.getenv('PT_HPU_LAZY_MODE') == '1'
-
-def mark_step(device):
-    if device == 'hpu' and is_lazy_mode():
-        htcore.mark_step()
+        return self.activation(x)
 
 def run(device='hpu', skip_torch_compile=False):
     # Create an instance of the model
     model = TanhModel()
     model = model.to(device)
-    if device=='hpu' and not is_lazy_mode() and not skip_torch_compile:
+    if os.getenv('PT_HPU_LAZY_MODE') != '1' and device=='hpu' and not skip_torch_compile:
         print('Running torch.compile with hpu_backend')
         model = torch.compile(model, backend='hpu_backend')
         print(f'model = {model}')
 
-    # Hyper-parameters
-    learning_rate = 0.01
-    loss_func = nn.MSELoss()
-
     # Example input tensor
-    example_input = torch.tensor([1.0, -1.0, 0.0, 0.5]).to(device)
-    target = torch.tensor([0.0]).to(device)
+    example_input = torch.tensor([1.0, -1.0, 0.0, 0.5], requires_grad=True).to(device)
 
     # Forward pass through the model
     output = model(example_input)
     print(f'output = {output}')
-
-    # Backward pass
-    loss = loss_func(output, target)
-    print(f'loss = {loss}')
-    loss.backward()
-    mark_step(device)
-    
-    for name, param in model.named_parameters():
-        if param.grad is not None:
-            print(f"Gradient of {name}: {param.grad}")
-
-    
 
 def get_args():
     parser = argparse.ArgumentParser(description='Args for run script')
