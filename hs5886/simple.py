984aa923083c8b49d3ebe2398aae09999af57c9c
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2025-04-21 18:55:43 +0000

    Add device switch to simple reproducer

--- hs5886/simple.py
+++ hs5886/simple.py
@@ -1,14 +1,25 @@
 import torch
 import random
+import os
 import numpy as np
+
+def get_device():
+    return os.getenv("DEVICE", "hpu")
+
+def get_compile_backend():
+    device = get_device()
+    compile_backend = 'hpu_backend' if device=='hpu' else 'inductor'
+    print(f'torch.compile config: device = {device} .. compile_backend = {compile_backend}')
+    return compile_backend
 
  ## @torch.compiler.disable
 class SimpleTest:
     def __init__(self, index):
         self.step_index = index if (index >= 0) else None
-        self.sigmas = torch.rand(10, device='hpu')
+        self.device = get_device()
+        self.sigmas = torch.rand(10, device=self.device)
  
-    @torch.compile(backend='hpu_backend')
+    @torch.compile(backend=get_compile_backend())
     def step(self):
         sigma = self.sigmas[self.step_index]
         sigma_next = self.sigmas[self.step_index + 1]
@@ -28,6 +39,6 @@ np.random.seed(seed)
 idx = 4
 st = SimpleTest(idx)
 x = st.step()
-#print(f'x = {x}')
+print(f'First step call: x={x}')
 y = st.step()
-#print(f'y = {y}')
+print(f'Second step call: y={y}')
