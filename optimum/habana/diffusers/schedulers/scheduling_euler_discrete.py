204b877a16dd04105d1e433e892ddc5d0edc9f4b
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2025-04-26 03:42:34 +0000

    Revert "Save potential changes to sdxl"
    This reverts commit 6848687dab01b36122272a19f654d0072efc9df3.

--- optimum/habana/diffusers/schedulers/scheduling_euler_discrete.py
+++ optimum/habana/diffusers/schedulers/scheduling_euler_discrete.py
@@ -152,7 +152,6 @@ class GaudiEulerDiscreteScheduler(EulerDiscreteScheduler):
             raise ValueError("Time-dependent parameters should be set first.")
         return
 
-    @torch.compiler.disable
     def scale_model_input(
         self, sample: torch.FloatTensor, timestep: Union[float, torch.FloatTensor]
     ) -> torch.FloatTensor:
