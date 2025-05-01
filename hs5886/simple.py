e66d2fc9f95d1dc44b336b4348cd93acb2e7e2a4
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2025-05-01 18:15:21 +0000

    Change simple test to match unit test we are checking in

--- hs5886/simple.py
+++ hs5886/simple.py
@@ -1,7 +1,5 @@
 import torch
-import random
 import os
-import numpy as np
 
 def get_device():
     return os.getenv("DEVICE", "hpu")
@@ -19,28 +17,24 @@ class SimpleTest:
     def __init__(self, index):
         self.step_index = index if (index >= 0) else None
         self.device = get_device()
-        self.sigmas = torch.rand(10, device=self.device)
+        self.sigmas = torch.arange(0, 16, device=self.device)
  
     @torch.compile(backend=get_compile_backend())
     def step(self):
         sigma = self.sigmas[self.step_index]
         sigma_next = self.sigmas[self.step_index + 1]
-        retval = sigma_next - sigma
+        retval = sigma_next + sigma
 
         # upon completion increase step index by one
         self.step_index += 1
 
         return retval
 
-torch.set_num_threads(1)
-seed = 42
-random.seed(seed)
-torch.manual_seed(seed)
-np.random.seed(seed)
-
 idx = 4
 st = SimpleTest(idx)
 x = st.step()
+assert x == 2*idx + 1
 print(f'First step call: x={x}')
 y = st.step()
+assert y == 2*idx + 3
 print(f'Second step call: y={y}')
