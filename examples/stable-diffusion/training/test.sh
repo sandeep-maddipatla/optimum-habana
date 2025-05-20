41b05f2b1d8965879eedb7a6cab24239f2e01845
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2025-05-20 02:24:08 +0000

    Save run scripts

---
+++ examples/stable-diffusion/training/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export MODEL_NAME="stable-diffusion-v1-5/stable-diffusion-v1-5"
+export INSTANCE_DIR="dog"
+export CLASS_DIR="path-to-class-images"
+export OUTPUT_DIR="out"
+
+PT_HPU_LAZY_MODE=0 python train_dreambooth.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --class_data_dir=$CLASS_DIR \
+  --with_prior_preservation --prior_loss_weight=1.0 \
+  --instance_prompt="a photo of sks dog" \
+  --class_prompt="a photo of dog" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --num_class_images=200 \
+  --mixed_precision=bf16 \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=5e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --max_train_steps=100 \
+  --gaudi_config_name Habana/stable-diffusion \
+ boft
+
+ 
