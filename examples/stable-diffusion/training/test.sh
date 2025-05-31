f168487e5ef18ca52a2e500f7dcc608c6a4e4cc3
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2025-05-31 04:40:12 +0000

    Revert "TOWL logs settings"
    This reverts commit f541ec2089ac6b31cfa090c59fe47915be8e2800.

--- examples/stable-diffusion/training/test.sh
+++ examples/stable-diffusion/training/test.sh
@@ -6,21 +6,6 @@ export OUTPUT_DIR="out"
 export PT_HPU_LAZY_MODE=${PT_HPU_LAZY_MODE:-0}
 export TRAIN_STEPS=${TRAIN_STEPS:-100}
 
-if [ "${PT_TOWL_LOG_ENABLE}" == "1" ]; then
-  echo "PT_TOWL_LOG_ENABLE=${PT_TOWL_LOG_ENABLE}. Including additional env variables"
-  export PT_HPU_ENABLE_EXECUTION_THREAD_NO_WAIT=1
-  export PT_HPU_EAGER_4_STAGE_PIPELINE_ENABLE=0
-  export PT_HPU_EAGER_PIPELINE_ENABLE=0
-  export PT_HPU_DISABLE_ASYNC_COLLECTIVE=1
-  export PT_HPU_ENABLE_LAZY_EAGER_LAUNCH_EXEC_THREAD=0
-  export PT_HPU_ENABLE_LAZY_EAGER_EXECUTION_THREAD=0
-  export PT_HPU_ENABLE_COMPILE_THREAD=0
-  export PT_HPU_ENABLE_EXECUTION_THREAD=0
-  export PT_HPU_LAZY_ACC_PAR_MODE=0
-  export PT_HPU_SYNC_LAUNCH=1
-  env
-fi
-
 python train_dreambooth.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --instance_data_dir=$INSTANCE_DIR \
