104e221f870b1fa62e3b6ecb7ba8fbee19914842
sandeep.maddipatla@intel.com <sandeep.maddipatla@intel.com>
2025-06-02 20:05:05 +0000

    Add CPU and memory utilization logs

--- examples/stable-diffusion/training/run_and_log.sh
+++ examples/stable-diffusion/training/run_and_log.sh
@@ -13,6 +13,18 @@ make_unique_dir() {
   echo "$dir"
 }
 
+prep_cpu()
+{
+    echo enable cpu performance governor
+    #expects linux-tools-common, linux-tools-generic packages installed for cpupower tool
+    CPU_POWER_TOOL_PATH=`find /usr/lib -executable -name cpupower | head -n 1`
+    export cpu_count=0
+    [ ! -x ${CPU_POWER_TOOL_PATH} ] && echo "cpupower tool not found" && exit
+    echo Found tool at $CPU_POWER_TOOL_PATH
+    cpu_count=`${CPU_POWER_TOOL_PATH} frequency-set --governor performance | wc -l`
+    echo Set Frequency for ${cpu_count} CPUs
+}
+
 run() {
     tag=result
     result_dir=$(make_unique_dir ${tag})
@@ -24,13 +36,27 @@ run() {
     echo Running tensorboard as background process $tb_pid
 
     pkill hl-smi
-    hl-smi -l 1 -Q "timestamp,name,bus_id,driver_version,temperature.aip,utilization.aip,memory.total,memory.free,memory.used,pcie.link.gen.max,pcie.link.gen.current,pcie.link.width.max" -f csv > hlsmi.csv 2>&1 &
+    hl-smi -l 1 -Q "timestamp,name,bus_id,driver_version,temperature.aip,utilization.aip,memory.total,memory.free,memory.used,pcie.link.gen.max,pcie.link.gen.current,pcie.link.width.max" -f csv > ${result_dir}/hlsmi.csv 2>&1 &
     hlsmi_pid=$(echo $!)
     echo Running hl-smi as background process $hlsmi_pid
 
-    export HABANA_PROFILE=1
-    hl-prof-config -e off --hw-trace off
-    hl-prof-config -e off --phase=device-acq -b 256 --skipParse On
+    if [ "${HABANA_PROFILE}" == "1" ]; then
+      hl-prof-config -e off --hw-trace off
+      hl-prof-config -e off --phase=device-acq -b 256 --skipParse On
+    fi
+
+    refresh_interval=1
+    pkill mpstat
+    mpstat -P ALL ${refresh_interval} > ${result_dir}/mpstat.log &
+    mpstat_pid=$(echo $!)
+
+    pkill free
+    free --seconds ${refresh_interval} --mega --total --wide > ${result_dir}/free.log &
+    free_pid=$(echo $!)
+
+    pkill vmstat
+    vmstat 1 > ${result_dir}/vmstat.log &
+    vmstat_id=$(echo $!)
 
     # Actual run
     cmd="$*"
@@ -46,12 +72,13 @@ run() {
     cp *.py  ${result_dir}
     cp *.log  ${result_dir} 2>/dev/null
     cp $0 ${result_dir} 2>/dev/null
-    mv hlsmi.csv ${result_dir} 2>/dev/null
     mv metricslog.json ${result_dir} 2>/dev/null
     mv *.hltv ${result_dir}
     chmod -R 777 ${result_dir}
     pkill tensorboard
     pkill hl-smi
+    pkill mpstat
+    pkill vmstat
 
     [ -d profile_logs ] && echo "Sleep 5s to ensure profiles are collected" && sleep 5
     [ -d profile_logs ] && mv profile_logs ${result_dir}
